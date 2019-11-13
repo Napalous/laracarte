5a8dc9a0602c9dca15076b208589c4fd1c4d09e9
napalous <napalousmanadda@gmail.com>
2019-11-13 10:25:12 +0100

    about&home

---
+++ resources/views/layouts/partials/_nav.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark static-top">
+        <a class="navbar-brand" href="{{ route('root_path') }}">Laracarte</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="{{ route('root_path') }}">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('about_path') }}">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Artisans</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Planet</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="dropdown-item" href="https://laravel.com">Laravel.com</a>
+                <a class="dropdown-item" href="https://laravel.io">Laravel.io</a>
+                <a class="dropdown-item" href="https://laracasts.com">Laracasts</a>
+                <a class="dropdown-item" href="https://larajobs.com">Larajobs</a>
+                <a class="dropdown-item" href="https://laravel-news.com">Laravel News</a>
+                <a class="dropdown-item" href="https://larachat.com">Larachat</a>
+              </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav navbar-right">
+              <li><a href="">Login</a></li>
+              <li><a href="">Register</a></li>
+          </ul>
+        </div>
+      </nav>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+    <h2 class="text-center">Map here!!!!!!!!!</h2>
+@endsection
